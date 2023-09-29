8a4116fbff1cf4f12b1f47d83a97487f857e9588
Shiv5268 <86187612+Shiv5268@users.noreply.github.com>
2023-09-29 22:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function firstNonRepeatedChar(str) {
         if(count[c])
             count[c]++;
         else
-            count[c)]=1;
+            count[c]=1;
     }
     for (let i=0;i<str.length;i++) {
-       let c=str[i]; 
-        if(count[c]===1){  
+		let c=str[i]; 
+		if(count[c]===1){  
             return c;
         }
     }
-    return null; 
+    return null;  
 }
 const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input));  
